04768450b34ce04d03d18b823947703bdbeaf6c2
Anant Chandela <anantchandela@gmail.com>
2026-04-09 12:19:06 +0530

    fix: handleSignin bug fixed, frontend-backend integration started

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://vishisht@localhost/ab_saas"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ab_saas"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
